fdf2325004673666436a5c8f20bae514ef19226e
ToxiChris <ogwuniruchris@gmail.com>
2022-03-13 14:44:40 +0100

    print alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+nclude <stdio.h>
+
+/**
+ * main - Prints the alphabetic
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char c;
+	char b;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (b = 'A'; b <= 'Z'; b++)
+		putchar(b);
+
+	putchar('\n');
+	return (0);
+}
